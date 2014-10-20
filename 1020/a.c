ddfbc4228283f3e48ae474f7fe7f3695b263cbbf
gwg <718324404@qq.com>
2014-10-21 00:24:48 +0800

    1020 first commit

---
+++ 1020/a.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: a.c
+    > Author: GWG
+    > Mail: gaowenguang123@gmail.com 
+    > Created Time: 2014年10月21日 星期二 00时17分26秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main(void)
+{
+    for(int i = 0;i < 10; i++)
+		printf("%d ",i);
+	printf("\n");
+	return 0;
+}
