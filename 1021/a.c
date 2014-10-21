6e99e0d724ccd1204fc5c1c4d5d4b9438d29bb49
gwg <718324404@qq.com>
2014-10-22 02:43:20 +0800

    1021 first commit

---
+++ 1021/a.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: a.c
+    > Author: GWG
+    > Mail: gaowenguang123@gmail.com 
+    > Created Time: 2014年10月22日 星期三 02时33分47秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main(void)
+{
+		printf("%d\n",1+2);
+		printf("%d\n",3-4);
+		printf("%d\n",5*6);
+		printf("%d\n",8/4);
+		printf("%d\n",8/5);
+	return 0;
+}
---
+++ 1021/b.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: b.c
+    > Author: GWG
+    > Mail: gaowenguang123@gmail.com 
+    > Created Time: 2014年10月22日 星期三 02时36分26秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main(void)
+{
+	printf("%d\n",8/5.0);
+    printf("%lf\n",8/5);
+	printf("%f\n",8/5.0);
+	printf("%lf\n",8/5.0);
+	printf("%.2lf\n",8/5.0);
+	return 0;
+}
