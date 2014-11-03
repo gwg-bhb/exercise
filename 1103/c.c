50850f1844f1b4c19d95558c1e22984a04e7ab3b
gwg <718324404@qq.com>
2014-11-04 00:21:50 +0800

    1103 first commit

---
+++ 1103/c.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+    > File Name: c.c
+    > Author: GWG
+    > Mail: gaowenguang123@gmail.com 
+    > Created Time: 2014年11月04日 星期二 00时16分52秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main(void)
+{
+	int card_count = 11;
+	while(card_count > 0)
+	{
+		puts("我绝不在课堂上写代码");
+		card_count --;
+	}
+	return 0;
+}
