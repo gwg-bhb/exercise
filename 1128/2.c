81b5e20a34664efa4f75e59a6c05e58800ef22f5
gwg <718324404@qq.com>
2014-11-29 00:38:55 +0800

    1128 first commit

---
+++ 1128/2.c
@@ -0,0 +1,30 @@
+/*************************************************************************
+    > File Name: 1.c
+    > Author: GWG
+    > Mail: gaowenguang123@gmail.com 
+    > Created Time: 2014年11月28日 星期五 23时20分53秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+	float latitude;
+	float longitude;
+	char info[80];
+	int start = 0;
+
+	puts("data=[");
+	while(3 == scanf("%f %f %79[^\n]", &latitude, &longitude, info))
+	{
+		if(start)
+			printf(",\n");
+		else
+		   start = 1;
+		printf("{latitude: %f, longitude: %f, info: %s}", latitude, longitude, info);
+	}
+	puts("\n]");
+	return 0;
+}
