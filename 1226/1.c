dfd9681a445533b652977198a427c67e8d746e08
GWG <gaowenguang123@gmail.com>
2014-12-27 02:40:52 +0800

    1226 first commit

---
+++ 1226/1.c
@@ -0,0 +1,27 @@
+/*************************************************************************
+    > File Name: 1.c
+    > Author: GWG
+    > Mail: 718324404@qq.com 
+    > Created Time: 2014年12月27日 星期六 02时25分49秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *f_in = fopen("12306.txt", "r");
+	FILE *f_out = fopen("j.txt", "w");
+
+	int count = 0;
+	char buf[200];
+	while(NULL != fgets(buf, 200, f_in))
+	{
+		count++;
+		fprintf(f_out, "%s", buf);
+		fflush(f_out);
+        printf("%d\n", count);
+	}
+	printf("count = %d\n", count);
+	fclose(f_in);
+	fclose(f_out);
+}
