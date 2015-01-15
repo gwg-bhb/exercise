38d36a0f476a4c4793607033f8c7c61ad48782b2
gwg-bhb <gaowenguang123@gmail.com>
2015-01-15 21:39:10 +0800

    20150115 first commit

---
+++ 20150115/1.c
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: 1.c
+    > Author: GWG
+    > Mail: 718324404@qq.com 
+    > Created Time: 2015年01月15日 星期四 21时27分38秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+
+	unsigned int i = 0;
+    srand(12);
+	i = rand();
+
+	printf("i = %d\n", i);
+	return 0;
+}
+
